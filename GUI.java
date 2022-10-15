706709d62f98636ea353559c28fe2269c3b1bd7c
Bryden Li <brydenli.97@gmail.com>
2022-10-15 17:42:04 -0600

    Add button and label

--- GUI.java
+++ GUI.java
@@ -1,6 +1,8 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory; 
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
@@ -11,8 +13,13 @@ public class GUI {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
 
+        JButton button = new JButton("Button");
+        JLabel label = new JLabel("Number of clicks: 0");
+
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
